0984d1a2841c0318bdaee888755fab38e1c46f03
Ольга Шатилова <elabuga-dar@yandex.ru>
2023-04-12 23:04:28 +0700

    Добавлены модели: Сategory, Genre, GenreTitle, Title; добавлены CastomMixin, фильтр TitleFilter, CategorySerializer, GenreSerializer, TitleReadSerializer, TitleWriteSerializer; Вьюсеты: CategoryViewSet, GenreViewSet, TitleViewSe; чтение из файлов в reviews/management/commands/_loaders.py.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,13 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
+
+from .views import (CategoryViewSet,
+                    TitleViewSet,
+                    GenreViewSet)
 
 app_name = 'api'
+
+router = DefaultRouter()
+router.register('titles', TitleViewSet, basename='titles')
+router.register('genres', GenreViewSet, basename='genres')
+router.register('categories', CategoryViewSet, basename='categories')
