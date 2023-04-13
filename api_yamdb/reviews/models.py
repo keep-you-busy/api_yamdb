2462a9fda6acfb6d5487762dbc7f7d8a0e15d4fd
Алексей Сидоров <aleksid92@gmail.com>
2023-04-13 23:57:20 +0700

    feature/communicate_version1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    score = models.SmallIntegerField(
+        validators=(MinValueValidator(1),
+                    MaxValueValidator(10)),
+        error_messages={'validators': 'Оценка должна быть от 1 до 10'}
+    )
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
