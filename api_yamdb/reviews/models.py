4d41e06d4828b17d0e095e1c9b6530ec69057a79
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-14 06:55:48 +0700

    Custom User model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+USER = 'user'
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+
+class User(AbstractUser):
+    CHOICES = [
+        (USER, 'Аутентифицированный пользователь'),
+        (ADMIN, 'Администратор'),
+        (MODERATOR, 'Модератор'),
+    ]
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        null=False
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        blank=True
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True
+    )
+    role = models.CharField(
+        'Роль',
+        max_length=20,
+        choices=CHOICES,
+        default=USER
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь',
+        verbose_name_plural = 'Пользователи',
+
+    def __str__(self) -> str:
+        return self.username
