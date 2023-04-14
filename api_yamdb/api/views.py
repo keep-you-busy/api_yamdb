893167e907aeb1b45d6dc074efd152177e108aef
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-14 07:08:55 +0700

    Auth & Users functional

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import filters, mixins, permissions, viewsets
+from reviews.models import User
 
-# Create your views here.
+from .serializers import GetTokenSerializer, SingUpSerializer, UsersSerializer
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,
+                          permissions.IsAdminUser)
+    serializer_class = UsersSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+
+
+class SingUpViewSet(viewsets.ViewSet):
+    serializer_class = SingUpSerializer
+    permission_classes = permissions.AllowAny
+
+class GetTokenViewSet(viewsets.ViewSet):
+    serializer_class = GetTokenSerializer
