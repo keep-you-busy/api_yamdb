3e8b8c8c2cd12f5d232b98900a2c2f01b2856b6b
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-14 07:05:35 +0700

    Users, singup and get token urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,17 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
-app_name = 'api'
+from .views import GetTokenViewSet, SingUpViewSet, UsersViewSet
+
+router_v1 = DefaultRouter()
+
+router_v1.register(
+    'users', UsersViewSet, basename='users'
+)
+
+
+urlpatterns = [
+    path('v1/auth/singup/', SingUpViewSet),
+    path('v1/auth/token/', GetTokenViewSet),
+    path('v1/', include(router_v1.urls)),
+]
