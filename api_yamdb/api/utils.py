17bf0d90cc63f8f94d61524bccd0f2bc76fdbfdf
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-17 21:41:01 +0700

    Tokens functional

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -1,4 +1,21 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.functional import SimpleLazyObject
+from rest_framework_simplejwt.tokens import RefreshToken
 
-make_token = SimpleLazyObject(lambda: PasswordResetTokenGenerator().make_token)
+token_generator = PasswordResetTokenGenerator()
+
+
+def make_token(user):
+    """Function allows to manually make a token for the user."""
+    return token_generator.make_token(user)
+
+
+def check_token(user, token):
+    """Function allows to manually check a token for the user."""
+    return token_generator.check_token(user, token)
+
+
+def get_token_for_user(user):
+    """Function allows to manually make a jwt token for the user."""
+    refresh = RefreshToken.for_user(user)
+
+    return str(refresh.access_token)
