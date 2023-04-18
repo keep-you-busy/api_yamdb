25aee81cb0ab0066c35395b5a5b499a2376996d1
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-18 17:23:49 +0700

    Revert "Merge pull request #1 from keep-you-busy/feature/communicate"
    This reverts commit eb07981ba59d67a688ab8a1c37b316b8c9ad44ed, reversing
    changes made to 41bb2e0d1f82f1d0b5942f47605e960330ed4581.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,18 +1,4 @@
 from django.urls import include, path
-from rest_framework import routers
-from .views import CommentViewSet, ReviewViewSet
+from rest_framework.routers import SimpleRouter
 
 app_name = 'api'
-
-v1_router = routers.DefaultRouter()
-v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments')
-v1_router.register(r'titles/(?P<title_id>\d+)/reviews',
-                   ReviewViewSet, basename='reviews')
-
-urlpatterns = [
-    path('v1/', include(v1_router.urls)),
-
-]
