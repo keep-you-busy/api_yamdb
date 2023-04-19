fed566a9c33272768a9b9b576bf477f20f602dad
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-19 15:59:04 +0700

    PEP8 fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CommentViewSet, GetTokenView, ReviewViewSet, SignUpView,
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    GetTokenView, ReviewViewSet, SignUpView, TitleViewSet,
                     UsersViewSet)
 
 router_v1 = DefaultRouter()
 
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register(
     'users',
     UsersViewSet,
