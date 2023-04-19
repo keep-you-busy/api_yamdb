2f8b966a7795886ac1645e8826b1df3bea33a02f
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-19 16:52:52 +0700

    New permissions due the main task

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,18 +1,49 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
+from rest_framework import permissions
 
 
-class AdminModeratorAuthorPermission(BasePermission):
-    """Редактировать и удалять пост может только владелец."""
+class IsReadOnly(permissions.BasePermission):
+    """Read only permission."""
 
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated
-                )
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsModerator(permissions.BasePermission):
+    """Moderator role permission."""
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            user = request.user
+            return user.is_moderator or user.is_admin or user.is_superuser
+        return False
+
+
+class IsAdministrator(permissions.BasePermission):
+    """Administrator role permission."""
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            user = request.user
+            return user.is_admin or user.is_superuser
+        return False
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """Only owner, moderator & admins can edit or delete object permission."""
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in SAFE_METHODS
-            or obj.author == request.user
-            or request.user.is_moderator
-            or request.user.is_admin
-        )
+        return (request.user.is_admin or request.user.is_moderator
+                or request.user.is_superuser or obj.author == request.user)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_authenticated:
+            return request.user.is_admin
+        return False
