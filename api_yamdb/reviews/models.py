d548a438042e82d314bc2733ec4a70a7edbd2438
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-24 16:14:49 +0700

    Fix in user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -22,16 +22,6 @@ class User(AbstractUser):
         'Адрес электронной почты',
         unique=True,
     )
-    first_name = models.CharField(
-        'Имя',
-        max_length=150,
-        blank=True
-    )
-    last_name = models.CharField(
-        'Фамилия',
-        max_length=150,
-        blank=True
-    )
     bio = models.TextField(
         'Биография',
         blank=True
